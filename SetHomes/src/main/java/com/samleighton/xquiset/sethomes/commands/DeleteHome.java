ab644ce25e2fb00878c6816a473e5f7df563707d
koirdsuzu <155215481+koirdsuzu@users.noreply.github.com>
2024-10-26 02:40:50 +0900

    日本語に変更

--- SetHomes/src/main/java/com/samleighton/xquiset/sethomes/commands/DeleteHome.java
+++ SetHomes/src/main/java/com/samleighton/xquiset/sethomes/commands/DeleteHome.java
@@ -18,52 +18,52 @@ public class DeleteHome implements CommandExecutor {
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        //Make sure the sender of the command is a player
+        //コマンドの送信者がプレイヤーであることを確認する
         if (!(sender instanceof Player)) {
-            //Sends message to sender of command that they're not a player
+            //コマンドの送信者にプレイヤーではないというメッセージを送信する
             ChatUtils.notPlayerError(sender);
             return false;
         }
 
-        //The player that sent the command
+        //コマンドを送信したプレイヤー
         Player p = (Player) sender;
         if (cmd.getName().equalsIgnoreCase("delhome")) {
-            //Check for proper permissions
+            //適切な権限があるか確認する
             if (!p.hasPermission("homes.delhome")) {
                 ChatUtils.permissionError(p);
                 return false;
             }
 
-            //The uuid of the player that sent the command
+            //コマンドを送信したプレイヤーのUUID
             String uuid = p.getUniqueId().toString();
 
-            //Check if they're trying to delete the default home or a named home base on parameters entered
+            //入力されたパラメータに基づいて、デフォルトのホームまたは名前付きホームを削除しようとしているかどうかを確認します。
             if (args.length < 1) {
-                //Make sure they have a home before we try and delete one
+                //削除する前に、Homeがあるかどうか確認します
                 if (!pl.hasUnknownHomes(uuid)) {
-                    ChatUtils.sendError(p, "No Default Home is currently set!");
+                    ChatUtils.sendError(p, "現在、デフォルトのホームが設定されていません。");
                     return true;
                 }
 
-                //Delete the players default home
+                //プレイヤーのデフォルトのホームを削除する
                 pl.deleteUnknownHome(uuid);
-                ChatUtils.sendSuccess(p, "Default Home has been removed!");
+                ChatUtils.sendSuccess(p, "デフォルトのホームが削除されました。");
                 return true;
             } else if (args.length > 1) {
-                //Tell the player if they've sent to many arguments with the command
+                //プレイヤーに、コマンドで引数が多すぎるかどうかを伝える
                 ChatUtils.tooManyArgs(p);
                 return false;
             } else {
-                //Check if they have any named homes or a home with the given name
+                //名前の付いたホームや、指定された名前のホームがあるかどうかを確認します
                 if (!(pl.hasNamedHomes(uuid)) || !(pl.getPlayersNamedHomes(uuid).containsKey(args[0]))) {
-                    ChatUtils.sendError(p, "You have no homes by that name!");
+                    ChatUtils.sendError(p, "そんな名前の家はありません。");
                     return true;
                 }
 
-                //Delete the home with the given name
+                //指定された名前のホームを削除します
                 pl.deleteNamedHome(uuid, args[0]);
-                //Tell the player which home they have deleted
-                ChatUtils.sendSuccess(p, "You have deleted the home: " + args[0]);
+                //プレイヤーにどの家を削除したかを伝える
+                ChatUtils.sendSuccess(p, "ホームを削除しました:" + args[0]);
                 return true;
             }
         }
@@ -75,49 +75,45 @@ public class DeleteHome implements CommandExecutor {
                 return false;
             }
 
-            //Check for the correct range of argument numbers
+            //引数の数値の範囲が正しいか確認する
             if (args.length < 1 || args.length > 2) {
-                ChatUtils.sendError(p, "ERROR: Incorrect number of arguments!");
+                ChatUtils.sendError(p, "エラー: 引数の数が正しくありません。");
                 return false;
             }
 
-            //Create new offline player for the player name they entered
+            //入力したプレーヤー名で新しいオフラインプレーヤーを作成する
             @SuppressWarnings({"deprecated"})
             OfflinePlayer targetP = Bukkit.getServer().getOfflinePlayer(args[0]);
-            //Check to be sure the player has played on the server before
+            //プレイヤーが以前にサーバーでプレイしたことがあるかどうかを確認してください
             if (!targetP.hasPlayedBefore()) {
-                ChatUtils.sendError(p, "The player " + ChatColor.WHITE + ChatColor.BOLD + args[0] + ChatColor.DARK_RED + " has never played here before!");
+                ChatUtils.sendError(p, "このプレイヤーは" + ChatColor.WHITE + ChatColor.BOLD + args[0] + ChatColor.DARK_RED + "これまでにサーバーに参加していません。");
                 return false;
             }
 
             String uuid = targetP.getUniqueId().toString();
-            //If only player name given try deleting the default home
+            //プレイヤー名のみが指定されている場合は、デフォルトのホームを削除してみてください
             if (args.length == 1) {
-                //Check to see if the player has a default home set before we try to delete it
+                //削除する前に、プレーヤーにデフォルトのホームセットがあるかどうかを確認します。
                 if (!pl.hasUnknownHomes(uuid)) {
-                    ChatUtils.sendError(p, "The player " + ChatColor.WHITE + ChatColor.BOLD + args[0] + ChatColor.DARK_RED + " has no default home!");
+                    ChatUtils.sendError(p, "このプレイヤーは" + ChatColor.WHITE + ChatColor.BOLD + args[0] + ChatColor.DARK_RED + "デフォルトのホームがありません。");
                     return false;
                 } else {
-                    //Perform deletion of the un-named home
+                    //名前のないホームの削除を実行する
                     pl.deleteUnknownHome(uuid);
-                    ChatUtils.sendSuccess(p, "You have deleted the default home for player " + ChatColor.WHITE + ChatColor.BOLD + args[0] + ChatColor.GOLD + "!");
+                    ChatUtils.sendSuccess(p, "プレーヤーのデフォルトのホームを削除しました。" + ChatColor.WHITE + ChatColor.BOLD + args[0] + ChatColor.GOLD + "!");
                     return true;
                 }
             } else {
-                //Attempt to find a named home with the named passed to us
+                //選択したホームがあるかの確認をする
                 String homeName = args[1];
                 if (!(pl.hasNamedHomes(uuid)) || !(pl.getPlayersNamedHomes(uuid).containsKey(homeName))) {
-                    ChatUtils.sendError(p, "The player " + ChatColor.WHITE + ChatColor.BOLD + args[0] + ChatColor.DARK_RED + " has no homes by that name!");
+                    ChatUtils.sendError(p, "このプレイヤーは" + ChatColor.WHITE + ChatColor.BOLD + args[0] + ChatColor.DARK_RED + "その名前のホームがありません。");
                     return false;
                 } else {
-                    //Perform deletion of the named home
+                    //指定されたホームの削除を実行します
                     pl.deleteNamedHome(uuid, homeName);
-                    ChatUtils.sendSuccess(p, "You have deleted the '" + homeName + "' home for player " + ChatColor.WHITE + ChatColor.BOLD + args[0] + ChatColor.GOLD + "!");
+                    ChatUtils.sendSuccess(p, "プレーヤーの'" + homeName + "'ホームを削除しました。" + ChatColor.WHITE + ChatColor.BOLD + args[0] + ChatColor.GOLD + "!");
                     return true;
                 }
             }
-        }
-        return false;
-    }
-
-}
+            
